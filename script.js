505bb7093b6d27311bfb065237cd8503cb97849a
sthalla <shravankumar.thalla@ellucian.com>
2025-07-01 15:30:50 -0400

    modified

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-// Minimal script (can extend later for animations)
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Shravan's site loaded!");
+  console.log("Site loaded with animation!");
 });
